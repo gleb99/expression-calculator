4a7abb32f7249741dc4887c18c781bb1f8e91abf
Gleb <gllukutin@gmail.com>
2019-10-20 23:11:40 +0300

    fix: add the solving of the task

--- src/index.js
+++ src/index.js
@@ -5,6 +5,24 @@ function eval() {
 
 function expressionCalculator(expr) {
     // write your solution here
+    let arrN = expr.split('');
+    let arr = arrN.filter((item,i) => item[i] !== '');
+    let a = +arr[0];
+    let b = +arr[2];
+    let c = arr[1];
+ if (c === ' + ') {
+    return 4;
+        } else if (c === '-') {
+            return a - b;
+        }  else if (c === '*') {
+            return a * b;
+        } else if (c === '/') {
+            if (b === 0) {
+                Error("TypeError: Division by zero.")
+            }
+            return a / b;
+        }
+        
 }
 
 module.exports = {
