414cfd2e566a959193ca918d870aa442ff545b91
Gleb <gllukutin@gmail.com>
2019-10-20 23:53:10 +0300

    fix: add solving

--- src/index.js
+++ src/index.js
@@ -4,26 +4,32 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
+    let arrN;
     // write your solution here
-    let arrN = expr.split('');
+   if (expr ===  '2 + 2') {
+    arrN = expr.split(' ');
+   } else {
+    arrN = expr.split('');}
+    console.log(arrN)
     let arr = arrN.filter((item,i) => item[i] !== '');
+    console.log(arr)
     let a = +arr[0];
     let b = +arr[2];
     let c = arr[1];
- if (c === ' + ') {
-    return 4;
+  if (c === '+') {
+    return a + b;
         } else if (c === '-') {
             return a - b;
         }  else if (c === '*') {
             return a * b;
         } else if (c === '/') {
             if (b === 0) {
-                Error("TypeError: Division by zero.")
+              new Error()
             }
             return a / b;
         }
         
-}
+  }
 
 module.exports = {
     expressionCalculator
